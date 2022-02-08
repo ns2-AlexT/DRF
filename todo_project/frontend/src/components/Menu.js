90e87fc931f0d61b00e6b7b5f50f3600299a2111
ns2-AlexT <P$20!20$eTsT%!557>
2022-02-08 23:35:16 +0700

    Lesson_2

---
+++ todo_project/frontend/src/components/Menu.js
@@ -0,0 +1,26 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Menu = () => {
+    return (
+        <ul className="header">
+            <li>
+                <a href="#">
+                    Users
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Tasks
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    Log in
+                </a>
+            </li>
+        </ul>
+)
+}
+
+export default Menu
