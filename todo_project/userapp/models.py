636f46e948c3a86ae550710daec0799ccea06c64
ns2-AlexT <P$20!20$eTsT%!557>
2022-04-07 23:32:31 +0700

    Lesson_12

--- todo_project/userapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    """
-    redefine the email field as unique
-    """
-    e_mail = models.EmailField('email address', unique=True)
